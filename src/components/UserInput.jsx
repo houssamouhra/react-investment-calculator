9170df35ea99138f278a1cdc8d055d24885dd246
Houssam Ouhra <houssamblur04@gmail.com>
2025-09-20 04:57:26 +0100

    feat(ui): implement UserInput component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,24 @@
+export const UserInput = () => {
+  return (
+    <section id='user-input'>
+      <div className='input-group'>
+        <p>
+          <label for='initial'>Initial Investment</label>
+          <input type='number' id='initial' required />
+        </p>
+        <p>
+          <label for='annual'>Annual Investment</label>
+          <input type='number' id='annual' required />
+        </p>
+        <p>
+          <label for='expected'>Expected Revenue</label>
+          <input type='number' id='expected' required />
+        </p>
+        <p>
+          <label for='duration'>Duration</label>
+          <input type='number' id='duration' required />
+        </p>
+      </div>
+    </section>
+  );
+};
